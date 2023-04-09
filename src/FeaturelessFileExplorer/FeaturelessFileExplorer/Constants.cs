2018b7fd64e9f34b6ffc995bdefc2af0fe201a33
chrcar01 <chrisjcarter@gmail.com>
2023-04-09 11:37:20 -0700

    format cleanup

--- src/FeaturelessFileExplorer/FeaturelessFileExplorer/Constants.cs
+++ src/FeaturelessFileExplorer/FeaturelessFileExplorer/Constants.cs
@@ -1,9 +1,8 @@
-﻿namespace FeaturelessFileExplorer
+﻿namespace FeaturelessFileExplorer;
+
+internal static class Constants
 {
-    internal static class Constants
-    {
-        public const string DISPLAY_TYPE_PARENT_FOLDER = "parent-folder";
-        public const string DISPLAY_TYPE_FOLDER = "folder";
-        public const string DISPLAY_TYPE_FILE = "file";
-    }
+    public const string DISPLAY_TYPE_PARENT_FOLDER = "parent-folder";
+    public const string DISPLAY_TYPE_FOLDER = "folder";
+    public const string DISPLAY_TYPE_FILE = "file";
 }
